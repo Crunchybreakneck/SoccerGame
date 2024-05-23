49b04d6810b063b99a535399d802de7fbd20eaca
Crunchybreakneck <crunchybreakneck@gmail.com>
2024-05-23 17:21:11 -0400

    Render Field

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const canvas = document.getElementById("Game");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#17B169";
+ctx.fillRect(0, 0, 960, 540);
+ctx.beginPath();
+ctx.moveTo(480, 0);
+ctx.lineTo(480, 540);
+ctx.lineWidth = 3;
+ctx.strokeStyle = '#fff';
+ctx.stroke()
+ctx.beginPath();
+ctx.arc(480, 270, 100, 0, 5 * Math.PI);
+ctx.stroke();
