6765f7d882acb43c7458bad0e29d88a40850d1fd
Crunchybreakneck <crunchybreakneck@gmail.com>
2024-05-23 17:31:34 -0400

    add goalie boxes

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const canvas = document.getElementById("Game");
 const ctx = canvas.getContext("2d");
+// add grass
 ctx.fillStyle = "#17B169";
 ctx.fillRect(0, 0, 960, 540);
+// add half line
 ctx.beginPath();
 ctx.moveTo(480, 0);
 ctx.lineTo(480, 540);
 ctx.lineWidth = 3;
 ctx.strokeStyle = '#fff';
 ctx.stroke()
+// add center circle
 ctx.beginPath();
 ctx.arc(480, 270, 100, 0, 5 * Math.PI);
+ctx.stroke();
+// add left goalie box
+ctx.rect(0, 125, 150, 300);
+ctx.stroke();
+// add right goalie box
+ctx.rect(810, 125, 150, 300);
 ctx.stroke();
